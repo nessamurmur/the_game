979b39b23621ea6f730c96811e12639a7e8b1714
Nate West <natescott.west@gmail.com>
2015-05-03 16:11:42 -0500

    MOAR DRY characters controller

--- web/controllers/character_controller.ex
+++ web/controllers/character_controller.ex
@@ -6,6 +6,7 @@ defmodule TheGame.CharacterController do
 
   plug :scrub_params, "character" when action in [:create, :update]
   plug :get_user
+  plug :get_character when action in [:edit, :show, :update, :delete]
   plug :action
 
   def index(conn, _) do
@@ -28,13 +29,11 @@ defmodule TheGame.CharacterController do
   end
 
   def show(conn, %{"user_id" => user_key, "id" => id}) do
-    character = Repo.one(from c in Character, where: c.user_id == ^conn.assigns.user.id, where: c.id == ^id)
-    render conn, "show.json", character: character
+    render conn, "show.json", character: conn.assigns.character
   end
 
   def update(conn, %{"user_id" => user_key, "id" => id, "character" => character_params}) do
-    character = Repo.one(from c in Character, where: c.user_id == ^conn.assigns.user.id, where: c.id == ^id)
-    changeset = Character.changeset(character, character_params)
+    changeset = Character.changeset(conn.assigns.character, character_params)
 
     if changeset.valid? do
       character = Repo.update(changeset)
@@ -47,14 +46,17 @@ defmodule TheGame.CharacterController do
   end
 
   def delete(conn, %{"user_id" => user_key, "id" => id}) do
-    character = Repo.one(from c in Character, where: c.user_id == ^conn.assigns.user.id, where: c.id == ^id)
-
-    character = Repo.delete(character)
+    character = Repo.delete(conn.assigns.character)
     render(conn, "show.json", character: character)
   end
 
   defp get_user(conn, _) do
     user = Repo.one(from u in User, where: u.key == ^conn.params["user_id"])
     assign(conn, :user, user)
+  end
+
+  defp get_character(conn, _) do
+    character = Repo.one(from c in Character, where: c.user_id == ^conn.assigns.user.id, where: c.id == ^conn.params["id"])
+    assign(conn, :character, character)
   end
 end
