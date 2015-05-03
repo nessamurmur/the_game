aa667233b52352d918cef15e5b75ef904d7fe3db
Nate West <natescott.west@gmail.com>
2015-05-02 19:39:11 -0500

    Change the default home page

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TheGame.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "The Game"
   end
 end
